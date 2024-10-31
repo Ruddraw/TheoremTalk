21cea6de15d01eebb1ea0db5f1839415b208c318
AFIF ASAD RUDDRAW <afifasad1117@gmail.com>
2024-10-30 22:38:40 -0300

    created super user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserChangeForm
 # Create your views here.
+def register(request):
+  form = UserChangeForm()
+  return render(request, 'register.html', {'form': form})
