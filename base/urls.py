2880e08250a63f58d156ec3057267766852d75b4
AFIF ASAD RUDDRAW <afifasad1117@gmail.com>
2024-11-17 04:18:31 -0400

    cannot access the question page. showing 404 error

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'base'
+app_name = 'base'  # Make sure the app_name is correct
 
 urlpatterns = [
-  path('', views.home, name = "home")
+    path('', views.home, name="home"),  # Home page URL
+    path('questions/', views.QuestionListView.as_view(), name="question_list"),  # The list of questions
+    # Other URL patterns for question detail, create, etc.
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Question
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+#CRUD function
+class QuestionListView(ListView):
+  model = Question
+  template_name = 'template/base/question_list.html'  # Adjust the path if needed
+  context_object_name = 'questions'
